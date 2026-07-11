82f9bbdd93fc8837afa19a5c1e237127fff67a66
Ajit kohir <ajitkohir@gmail.com>
2026-07-11 21:18:55 +0900

    updating firebase

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,8 @@ import { BrowserRouter } from 'react-router-dom'
 import { i18nReady } from './i18n'
 import AuthProvider from './auth/AuthProvider'
 
+void import('./lib/firebase')
+
 void i18nReady.then(() => {
   ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
